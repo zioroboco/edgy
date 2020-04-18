18da79ff3df8fc081c14d61d3c888e2c24ca5c48
Louis Quinnell <_@ziorobo.co>
2020-04-18 19:59:50 +1000

    Run jest from within packages

--- packages/stack/jest.config.js
+++ packages/stack/jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testRunner: "jest-circus/runner",
   testMatch: ["**/*.spec.ts"],
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.base.json",
-    },
-  },
+  rootDir: __dirname,
 }
